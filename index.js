d995aae96461262c63da2b844d987a365e699b00
Thomas Genin <xt6.thomas.genin@gmail.com>
2016-03-09 15:14:34 -0800

    Copy over logic from https://github.com/genintho/coffee-loader

--- index.js
+++ index.js
@@ -4,23 +4,63 @@
 */
 var coffee = require("coffee-script");
 var loaderUtils = require("loader-utils");
+var crypto = require('crypto');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+
+var hasCreatedCacheDirectory = false
+
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
+	var loaderOptions = loaderUtils.parseQuery(this.query);
 	var coffeeRequest = loaderUtils.getRemainingRequest(this);
 	var jsRequest = loaderUtils.getCurrentRequest(this);
 	var query = loaderUtils.parseQuery(this.query);
 	var result;
+
 	try {
-		result = coffee.compile(source, {
-			literate: query.literate,
-			filename: coffeeRequest,
-			debug: this.debug,
-			bare: true,
-			sourceMap: true,
-			sourceRoot: "",
-			sourceFiles: [coffeeRequest],
-			generatedFile: jsRequest
-		});
+		var cacheDirectory = loaderOptions.cacheDirectory ? path.normalize( loaderOptions.cacheDirectory + path.sep) : false;
+
+		if (cacheDirectory && !hasCreatedCacheDirectory) {
+			mkdirp.sync(cacheDirectory);
+		}
+
+		if (cacheDirectory) {
+			var sourceMD5 = crypto.createHash("md5").update(source).digest("hex");
+			var cacheFile = path.normalize(cacheDirectory + path.sep + "coffe_loader_" + sourceMD5 + ".json");
+
+			try {
+				var cacheFileContent = fs.readFileSync(cacheFile);
+				if (cacheFileContent) {
+					result = JSON.parse(cacheFileContent);
+				}
+
+			} catch (e) {
+				result = coffee.compile(source, {
+					literate: query.literate,
+					filename: coffeeRequest,
+					debug: this.debug,
+					bare: true,
+					sourceMap: true,
+					sourceRoot: "",
+					sourceFiles: [coffeeRequest],
+					generatedFile: jsRequest
+				});
+				fs.writeFileSync(cacheFile, JSON.stringify(result));
+			}
+		} else {
+			result = coffee.compile(source, {
+				literate: query.literate,
+				filename: coffeeRequest,
+				debug: this.debug,
+				bare: true,
+				sourceMap: true,
+				sourceRoot: "",
+				sourceFiles: [coffeeRequest],
+				generatedFile: jsRequest
+			});
+		}
 	} catch (e) {
 		var err = "";
 		if (e.location == null || e.location.first_column == null || e.location.first_line == null) {
