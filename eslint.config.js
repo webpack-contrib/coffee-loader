2432ffd71e604efb4fc7194527dbc24b60829bee
Nitin Kumar <snitin315@gmail.com>
2025-07-11 14:30:24 +0530

    chore: remove duplicate ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import configWebpack from "eslint-config-webpack";
 
 export default defineConfig([
-  globalIgnores(["node_modules", "dist", "coverage", "test/fixtures"]),
   {
     extends: [configWebpack],
     languageOptions: {
