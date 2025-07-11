1d952401b92338f3f2c3c817a7f7856f01b6adb3
Nitin Kumar <snitin315@gmail.com>
2025-07-11 18:19:07 +0530

    chore: upgrade ESLint to v9 (#203)

--- test/helpers/execute.js
+++ test/helpers/execute.js
@@ -1,20 +1,18 @@
-import Module from "module";
-import path from "path";
+import Module from "node:module";
+import path from "node:path";
 
 const parentModule = module;
 
 export default (code) => {
   const resource = "test.js";
   const module = new Module(resource, parentModule);
-  // eslint-disable-next-line no-underscore-dangle
+
   module.paths = Module._nodeModulePaths(
     path.resolve(__dirname, "../fixtures"),
   );
   module.filename = resource;
 
-  // eslint-disable-next-line no-underscore-dangle
   module._compile(`module.exports = ${code};`, resource);
 
-  // eslint-disable-next-line no-underscore-dangle
   return module.exports.__esModule ? module.exports.default : module.exports;
 };
